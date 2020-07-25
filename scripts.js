b391ba35d9e97a0ccd169a7a70a20fb43c4c23f5
patrick <patrick204nqh@gmail.com>
2020-07-25 13:24:36 +0700

    animation scroll with header

---
+++ scripts.js
@@ -0,0 +1,10 @@
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  const header = document.querySelector('header')
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    header.classList.add('header-hidden')
+  } else {
+    header.classList.remove('header-hidden')
+  }
+}
