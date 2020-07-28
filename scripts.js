50a188af8b5635e4ed424adff565b7596836add4
patrick <patrick204nqh@gmail.com>
2020-07-29 00:03:05 +0700

    slider

--- scripts.js
+++ scripts.js
@@ -18,3 +18,33 @@ const navLink = document.getElementsByClassName('nav-link')[0];
 toggleButton.addEventListener('click', () => {
   navLink.classList.toggle('open')
 })
+
+// slideshow indicators
+
+window.onload = () => {
+  if (window.innerWidth < 820) {
+    showSlide(0);
+  }
+}
+
+let currentSlide = 0;
+
+window.onresize = () => {
+  if (window.innerWidth < 820) {
+    showSlide(currentSlide);
+  }
+}
+
+const showSlide = (current) => {
+  const slides = document.querySelectorAll('.slides');
+  currentSlide = current;
+  if (current + 1 > slides.length) { currentSlide = 0; }
+  if (current < 0) { currentSlide = slides.length - 1; }
+  console.log(current, slides.length, currentSlide)
+  slides.forEach(slide => slide.classList.remove('show'));
+  slides[currentSlide].classList.add('show');
+}
+
+const plusDiv = (n) => {
+  showSlide(currentSlide += n)
+}
